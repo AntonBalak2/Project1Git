8fb786c5b6706a516d41ec88da6ad78cf4d9be80
Balak.Anton <anton.balak@auk.edu.ua>
2024-08-30 10:35:05 +0300

    Initial commit

---
+++ src/Part1.java
@@ -0,0 +1,10 @@
+public class Part1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a = 19;
+        int b = 20;
+        System.out.println(a + b);
+        String cherry = "cherry";
+        System.out.println(cherry.toUpperCase());
+    }
+}
